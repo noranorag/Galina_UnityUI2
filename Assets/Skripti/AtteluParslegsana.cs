5a13becc8f904665aca484fe03637fef37236d6f
Nora <nora.galina@sk.lvt.lv>
2023-05-02 10:47:21 +0300

    Radita iespeja izveleties character

---
+++ Assets/Skripti/AtteluParslegsana.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParslegsana : MonoBehaviour {
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+
+        else if (skaitlis == 1)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+
+        else if (skaitlis == 2)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[2];
+        Debug.Log("Nav piesaistīts attēls!");
+    }
+
+}
